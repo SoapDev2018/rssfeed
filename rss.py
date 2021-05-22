28ceabb58270f6db3b194e4d94cbdbd527adfa94
bodysoda69 <72090512+bodysoda69@users.noreply.github.com>
2021-05-22 21:26:21 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -352,7 +352,7 @@ def check_feed13():
     entry = FEED.entries[0]
     if entry.id != db.get_link(feed_url13).link:
                    # ↓ Edit this message as your needs.
-      message = f"/bonk {entry.links[1]['href']}"
+      message = f"/get {entry.links[1]['href']}"
       try:
         app.send_message(log_channel, message)
         db.update_link(feed_url13, entry.id)
